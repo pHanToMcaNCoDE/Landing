d7e715a17d6372dfe08a9817dd0159dc0ece87f3
Victory Odumeh <vodumeh@gmail.com>
2024-02-20 01:18:34 +0100

    Added buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
+    <section>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+        <h1 className="text-4xl lg:text-6xl text-[#4fbfeb] font-mono tracking-widest leading-snug text-center">
+          Ingenre
+        </h1>
+        <p className="text-center tracking-wider text-[.8rem] md:text-lg lg:text-xl text-gray-300 font-mono leading-10">
+          Get a quick list of animes to watch
         </p>
+
+        <button className="bg-black text-white font-mono tracking-wider px-3 py-2 rounded-lg">
+          Click me
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </section>
+  );
 }
 
-export default App
+export default App;
